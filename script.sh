9cbdfaa9f8e055e8898baf1e7943f28146e31ff3
subash2628 <subashsharma2628@gmail.com>
2024-02-29 15:15:24 +0545

    script file updated subash26288/

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 TAG=$1
 
-sed "s|image: my-node-app:[a-zA-Z0-9.-]*|image: subash26288/my-node-app:$TAG|" compose.yaml > compose_tmp.yaml && mv compose_tmp.yaml compose.yaml
+sed "s|image: subash26288/my-node-app:[a-zA-Z0-9.-]*|image: subash26288/my-node-app:$TAG|" compose.yaml > compose_tmp.yaml && mv compose_tmp.yaml compose.yaml
