24f0507a1aabc7b7ef91c4c05e794883ec9d6d4c
subash2628 <subashsharma2628@gmail.com>
2024-02-29 14:42:20 +0545

    script will edit compose file

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build -t my-node-app:1.2 .
+
+TAG=$1
+
+sed "s|image: my-node-app:[a-zA-Z0-9.-]*|image: my-node-app:$TAG|" compose.yaml > compose_tmp.yaml && mv compose_tmp.yaml compose.yaml
